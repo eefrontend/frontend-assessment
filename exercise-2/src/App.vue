258fe36d02461f0d77d380167e1db488dfff671b
eanesparrago <eanesparrago@gmail.com>
2021-05-25 20:10:29 +0800

    Feat: Setup exercise-2

---
+++ exercise-2/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <base-accordion />
+  </div>
+</template>
+
+<script>
+import BaseAccordion from "./components/BaseAccordion.vue";
+
+export default {
+  name: "App",
+  components: {
+    BaseAccordion,
+  },
+};
+</script>
+
+<style></style>
---
+++ exercise-2/src/components/BaseAccordion.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>BaseAccordion</div>
+</template>
+
+<script>
+export default {
+  name: "BaseAccordion",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
